222d9513f7891977a70574bbdbecdefd95bb4442
mfloto <60036186+mfloto@users.noreply.github.com>
2023-03-17 19:14:24 +0100

    changed return

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,40 @@ mod tm_response;
 
 use std::env;
 
+#[derive(Debug, Clone)]
+struct resale_offer {
+    //TODO: add date fields (first seen, sold)
+    id: String,
+    price: u32,
+}
+
 #[tokio::main]
 async fn main() {
     let event_id = env::var("EVENT_ID").expect("EVENT_ID not set");
     //let discord_webhook = env::var("DISCORD_WEBHOOK").expect("DISCORD_WEBHOOK not set");
-    get_resale_offers(&event_id).await;
+    let current_offers = get_resale_offers(&event_id).await;
+    for offer in current_offers {
+        println!("{:?}", offer.id);
+    }
 }
 
-async fn get_resale_offers(resale_id: &str) {
+//TODO: return Result
+async fn get_resale_offers(resale_id: &str) -> Vec<tm_response::Offer> {
+    //TODO: make url configurable (e.g. for other countries)
     let res = reqwest::get(format!(
         "https://availability.ticketmaster.eu/api/v2/TM_DE/resale/{}",
         resale_id
     ))
     .await;
-    if res.is_err() {
-        println!("Error: {:?}", res.err());
-        return;
-    }
+
     let res = res.unwrap();
     if res.status() != 200 {
         println!("Error: {:?}", res.status());
-        return;
+        panic!("Call to Ticketmaster API failed");
     }
-    let tm_res = &res.json::<tm_response::TMRes>().await;
-    if tm_res.as_ref().is_err() {
-        println!("Error: {:?}", tm_res.as_ref().err());
-        return;
-    }
-    let offers = &tm_res.as_ref().unwrap().offers;
+    let tm_res = res.json::<tm_response::TMRes>().await;
+
+    let offers = tm_res.expect("Failed to parse json").offers;
     println!("Found {} offers \n", offers.len());
-    for offer in offers {
-        println!(
-            "id: {id}\nprice: {price} €\noriginal price: {org_price} €\n",
-            id = offer.id,
-            price = offer.price.total / 100,
-            org_price = offer.price.original / 100
-        );
-    }
+    offers
 }
--- src/tm_response.rs
+++ src/tm_response.rs
@@ -1,13 +1,13 @@
 use serde::Deserialize;
 
 ///Holds information about a ticketmaster event
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct TMRes {
     pub offers: Vec<Offer>,
 }
 
 ///Holds information about a resale offer
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[allow(non_snake_case)]
 pub struct Offer {
     pub id: String,
@@ -20,21 +20,21 @@ pub struct Offer {
     pub r#type: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct OfferLimit {
     pub min: u32,
     pub max: u32,
     pub multiple: u32,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct OfferPrice {
     pub total: u32,
     pub original: u32,
     pub commission: u32,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[allow(non_snake_case)]
 pub struct OfferSellerInformation {
     pub businessType: String,
