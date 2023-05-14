fee6f7e9cbf56d57c3d537cb91becec710a69033
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2023-05-14 23:14:13 +0200

    feat: Better error handling, dotenvy for non-docker development (#2)
    * Added better error handling, dotenvy for non-docker development
    
    * PR Fixes
    
    * Oops

--- src/main.rs
+++ src/main.rs
@@ -5,58 +5,65 @@ use std::env;
 
 use crate::discord::notify_discord_server;
 use crate::tm_response::{Offer, TMRes};
+use color_eyre::eyre::Context;
+use color_eyre::{Result, Report, Help};
 
 #[tokio::main]
-async fn main() {
-    // Get environment variables
-    let event_id = env::var("EVENT_ID").expect("EVENT_ID not set");
-    let discord_webhook = env::var("DISCORD_WEBHOOK").expect("DISCORD_WEBHOOK not set");
-    let country_code = env::var("COUNTRY_CODE").expect("COUNTRY_CODE not set");
+async fn main() -> Result<()> {
+    dotenvy::dotenv().ok();
+    color_eyre::install()?;
+    // Get environment variables like in the first line
+    let event_id = env::var("EVENT_ID").wrap_err("Unable to get EVENT_ID").suggestion("Check if EVENT_ID is correct in .env")?;
+    let discord_webhook = env::var("DISCORD_WEBHOOK").wrap_err("Unable to get DISCORD_WEBHOOK").suggestion("Check if DISCORD_WEBHOOK is correct in .env")?;
+    let country_code = env::var("COUNTRY_CODE").wrap_err("Unable to get COUNTRY_CODE").suggestion("Check if COUNTRY_CODE is correct in .env")?;
     let threshold_price = env::var("THRESHOLD_PRICE")
         .unwrap_or("0".to_string())
         .parse::<u32>()
         .expect("THRESHOLD_PRICE is not a number")
         * 100;
 
-    let current_offers = get_resale_offers(&event_id, &country_code).await;
+    let current_offers = get_resale_offers(&event_id, &country_code).await.wrap_err("Unable to get resale offers").suggestion("Check if Ticketmaster is up or blocked your IP")?;
     // Compare online offers with offers in database
-    let conn = get_db_conn();
+    let conn = get_db_conn().wrap_err("Unable to get database connection").suggestion("Check if data/resale.db exists and is a valid sqlite3 database")?;
     for offer in current_offers {
-        if is_offer_in_db(&conn, &offer) {
+        if is_offer_in_db(&conn, &offer)? {
             println!("Offer already exists: {:?}", offer.id);
         } else {
             println!("New offer: {:?}", offer.id);
             if threshold_price == 0 || offer.price.total <= threshold_price {
                 println!("Price is below threshold. Sending notification...");
-                notify_discord_server(&discord_webhook, &offer).await;
+                notify_discord_server(&discord_webhook, &offer)
+                    .await
+                    .unwrap_or_else(|e| println!("Failed to send notification because of {e}"));
             }
-            insert_offer_into_db(&conn, &offer);
+            insert_offer_into_db(&conn, &offer).wrap_err("Unable to insert offer into database").suggestion("Check if data/resale.db exists and is writeable")?;
         }
     }
+    Ok(())
 }
 
 //TODO: return Result
-async fn get_resale_offers(resale_id: &str, country_code: &str) -> Vec<Offer> {
+async fn get_resale_offers(resale_id: &str, country_code: &str) -> Result<Vec<Offer>> {
     let res = reqwest::get(format!(
         "https://availability.ticketmaster.eu/api/v2/TM_{country_code}/resale/{resale_id}"
     ))
     .await;
 
-    let res = res.unwrap();
+    let res = res?;
     if res.status() != 200 {
         println!("Error: {:?}", res.status());
-        panic!("Call to Ticketmaster API failed");
+        return Err(Report::msg("Could not get ticketmaster response"));
     }
-    let tm_res = res.json::<TMRes>().await;
+    let tm_res = res.json::<TMRes>().await?;
 
-    let offers = tm_res.expect("Failed to parse json").offers;
+    let offers = tm_res.offers;
     println!("Found {} offers \n", offers.len());
-    offers
+    Ok(offers)
 }
 
 /// Creates a new database connection and creates the offers table if it does not exist
-fn get_db_conn() -> rusqlite::Connection {
-    let conn = rusqlite::Connection::open("/data/resale.db").expect("Failed to open database");
+fn get_db_conn() -> Result<rusqlite::Connection> {
+    let conn = rusqlite::Connection::open("/data/resale.db")?;
     conn.execute(
         "CREATE TABLE IF NOT EXISTS offers (
                   id              TEXT PRIMARY KEY,
@@ -64,24 +71,22 @@ fn get_db_conn() -> rusqlite::Connection {
                   )",
         (),
     )
-    .expect("Failed to create table");
-    conn
+    ?;
+    Ok(conn)
 }
 
 /// Checks if an offer is already in the database
-fn is_offer_in_db(conn: &rusqlite::Connection, offer: &Offer) -> bool {
+fn is_offer_in_db(conn: &rusqlite::Connection, offer: &Offer) -> Result<bool> {
     let mut stmt = conn
-        .prepare("SELECT 1 FROM offers WHERE id = ?")
-        .expect("Failed to prepare statement");
-    stmt.exists([&offer.id])
-        .expect("Failed to execute statement")
+        .prepare("SELECT 1 FROM offers WHERE id = ?")?;
+    Ok(stmt.exists([&offer.id])?)
 }
 
 /// Inserts an offer into the database
-fn insert_offer_into_db(conn: &rusqlite::Connection, offer: &Offer) {
+fn insert_offer_into_db(conn: &rusqlite::Connection, offer: &Offer) -> Result<()> {
     conn.execute(
         "INSERT INTO offers (id, price) VALUES (?1, ?2)",
         [&offer.id, &offer.price.total.to_string()],
-    )
-    .expect("Failed to insert offer");
+    )?;
+    Ok(())
 }
